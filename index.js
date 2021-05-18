b9573cb4dc3b60ea0ffffb7d0ff40d7af9721ea9
jackcornblum <jcornblum@gmail.com>
2021-05-18 16:57:03 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 29;
+const num4 = 5;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let mod = num3 % num4;
+
+let max = Math.max(3,20,5,10);
+
